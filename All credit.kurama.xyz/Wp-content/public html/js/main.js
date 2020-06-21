94f12708c404dab105953aaf5e69a788fade5cbd
hawkie283 <57499451+hawkie283@users.noreply.github.com>
2020-06-22 01:32:49 +0300

    Add files via upload

---
+++ All credit.kurama.xyz/Wp-content/public html/js/main.js
@@ -0,0 +1,84 @@
+
+$(function() {
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 18000,
+    values: [ 300, 18000 ],
+    slide: function( event, ui ) {
+      $( "#amount" ).val(  + ui.values[ 0 ] );
+      $( "#amount-1" ).val( + ui.values[ 1 ] );
+    }
+  });
+  $( "#amount" ).val( + $( "#slider-range" ).slider( "values", 0 ) );
+    $( "#amount-1" ).val(  + $( "#slider-range" ).slider( "values", 1 ) );
+}());
+
+
+
+$('ul.tabs__caption').on('click', 'li:not(.active)', function(e) {
+  e.preventDefault();
+  $(this)
+    .addClass('active').siblings().removeClass('active')
+    .closest('section.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+});
+
+// owl carousel
+
+$(function () {
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    dots: false,
+    // navText: ['<img src="img/svg/left.svg">','<img src="img/svg/right.svg">'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1200:{
+          items:3
+        },
+        1440:{
+            items:4
+        }
+    }
+});
+});
+
+$(function () {
+  $('.owl-carousel-experts').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    dots: false,
+    // navText: ['<img src="img/svg/left.svg">','<img src="img/svg/right.svg">'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1200:{
+          items:3
+        },
+        1440:{
+            items:4
+        }
+    }
+});
+});
+
+// menu nav toggle
+
+
+$("#nav_toggle").on("click", function(event) {
+  event.preventDefault();
+
+  $(this).toggleClass("active");
+  $("#nav").toggleClass("active");
+});
